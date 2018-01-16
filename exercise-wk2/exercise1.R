ec86e12cb82f42a331d74cf8426550d0ba18f495
Daniel Lee <donloby@uw.edu>
2018-01-16 14:13:45 -0800

    Add wk2 exercise work

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -9,7 +9,8 @@ my.vector <- c(my.num, initials)
 vector.sum <- sum(my.vector)
 
 # Describe why this doesn't work: 
-# 
+# doesn't work because my.vector has a string in the vector, and you cannnot
+# vectors of different types. Basically, they are different types
 
 install.packages("stringr")
 
@@ -18,29 +19,43 @@ my.line <- "Hey, hey, this is the library"
 print(str_length(my.line))
 
 # Describe why this doesn't work: 
-# 
+# You have to load the library after you install it. Just installing the 
+# package does not mean that it is currently working. library("stringr")
+# will fix this problem.
 
 said.the.famous <- paste(my.line, " - ", initial)
 
 # Describe why this doesn't work: 
-# 
+# initial was not defined and therefore cannot be concatenated to the other
+# vectors
 
 
 ## Part 2 - Vector and function practice
 
 # Make a vector and use typeof to check what type R considers it to be
-
+number.list <- c(1, 2, 3, 4)
+typeof(number.list)
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-
+CompareLength <- function(first, second) {
+  return(paste("The difference in lengths is", abs(length(first) - length(second))))
+}
 
 # Pass two vectors of different length to your `CompareLength` function
-
+CompareLength(number.list, 1)
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
-
+DescribeDifference <- function(first, second) {
+  Diff <- length(first) - length(second)
+  if (Diff > 0) {
+    return(paste0("Your first vector is longer by ", Diff, " elements."))
+  } else {
+    return(paste0("Your second vector is longer by ", Diff, " elements."))
+  }
+  
+}
 # Pass two vectors to your `DescribeDifference` function
 
 
